6aecc05f4cc89de4b76262b9a5c0b31ba8fb6b10
chairco <chairco@gmail.com>
2018-05-21 21:25:01 +0800

    Update content

--- docs/conf.py
+++ docs/conf.py
@@ -101,7 +101,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'Channels-docs-zhTW'
+htmlhelp_basename = 'Channels-docs-zh_TW'
 
 
 # -- Options for LaTeX output ---------------------------------------------
